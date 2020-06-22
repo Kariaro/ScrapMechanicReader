66764b8808eeb7962049d2bbcd7dce529c2eb6b4
Admin <Admin@DESKTOP-SU6FN7H>
2020-06-22 14:23:28 +0200

    Added core files

---
+++ src/sm/hardcoded/tile/Tile.java
@@ -0,0 +1,7 @@
+package sm.hardcoded.tile;
+
+public class Tile {
+	public TileHeader header;
+	
+	// TODO - Data
+}
---
+++ src/sm/hardcoded/tile/TileReader.java
@@ -0,0 +1,18 @@
+package sm.hardcoded.tile;
+
+import java.io.IOException;
+
+public final class TileReader {
+	private TileReader() {
+		
+	}
+	
+	public static Tile read(String path) throws IOException {
+		
+		return null;
+	}
+	
+	public static void save(Tile tile, String path) throws IOException {
+		
+	}
+}
